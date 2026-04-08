90ac37bd8a4124b36da37f8e9bfd6bd3b82c3486
Ydemae <113693952+Ydemae@users.noreply.github.com>
2026-04-08 19:01:33 +0200

    Quick fixes
    - Change traefik version to support new docker versions
    - Change frontend environment

--- frontend/src/environments/environments.prod.ts
+++ frontend/src/environments/environments.prod.ts
@@ -3,6 +3,6 @@
 
 export const environment = {
     production: true,
-    apiUrl: 'https://api.manwhameter.com',
-    appUrl: 'https://manwhameter.com'
+    apiUrl: 'https://api.manwhameter.eu',
+    appUrl: 'https://manwhameter.eu'
 };
