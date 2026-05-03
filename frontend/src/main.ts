937b6ce3cea5427a583c79085ed8442752b05e4b
Ydemae <113693952+Ydemae@users.noreply.github.com>
2026-05-03 08:14:06 +0200

    upgrade: angular 21 migration

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,4 @@
+import { provideZoneChangeDetection } from "@angular/core";
 // Copyright (c) 2025 Ydemae
 // Licensed under the AGPLv3 License. See LICENSE file for details.
 
@@ -7,7 +8,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true,
-})
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection({ eventCoalescing: true })], })
   .catch(err => console.error(err));
